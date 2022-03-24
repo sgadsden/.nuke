9a631e291ec748ed6c0d10a8e3a0cc91c1b0ed33
Stephanie Gadsden <64226949+sgadsden@users.noreply.github.com>
2022-03-24 15:35:09 -0400

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,18 +1,20 @@
 
-# 2021
+# 2022
 #
 #
 import nuke
 import platform
 import cryptomatte_utilities
 cryptomatte_utilities.setup_cryptomatte_ui()
+import pixelfudger
+
 
 
 #-----Define nuke directory
 
-Win_Dir = 'C:\Users\sgadsden\.nuke'
+Win_Dir = 'C:\Users\ASUS\.nuke'
 Mac_Dir = ''
-Linux_Dir = '/home/sgadsden/.nuke'
+Linux_Dir = '/home/ASUS/.nuke'
 
 # Automatically set global directory
 if platform.system() == "Windows":
@@ -23,7 +25,6 @@ elif platform.system() == "Linux":
 	dir = Linux_Dir
 else:
 	dir = None
-
 
 #------Know Defaults
 nuke.knobDefault('Tracker4.shutteroffset', "centered")
@@ -36,8 +37,7 @@ nuke.knobDefault('MotionBlur3D.shutteroffset', "centered")
 nuke.knobDefault('ScanlineRender.shutteroffset', "centered")
 nuke.knobDefault('Card3D.shutteroffset', "centered")
 nuke.knobDefault('Merge2.bbox',"B")
-
-
+nuke.addOnUserCreate(lambda:nuke.thisNode()['first_frame'].setValue(nuke.frame()), nodeClass='FrameHold')
 
 # Create Utilities menu & assign items
 utilitiesMenu = nuke.menu('Nuke').addMenu('Utilities')
@@ -46,11 +46,7 @@ utilitiesMenu.addCommand('Autocrop', 'nukescripts.autocrop()')
 myGizmosMenu = nuke.menu('Nodes').addMenu('myGizmos', icon=dir+"/icons/myGizmos_icon.png")
 myGizmosMenu.addCommand('bm_CameraShake', 'nuke.createNode("bm_CameraShake")', icon="bm_CameraShake_icon.png")
 myGizmosMenu.addCommand('bm_EdgeMatte', 'nuke.createNode("bm_EdgeMatte")')
-myGizmosMenu.addCommand('Bloom', 'nuke.createNode("Bloom")')
-myGizmosMenu.addCommand('Aberration', 'nuke.createNode("Aberration")')
-myGizmosMenu.addCommand('bm_OpticalGlow', 'nuke.createNode("bm_OpticalGlow")')
-
-
+myGizmosMenu.addCommand('FlareFactory', 'nuke.createNode("FlareFactory")')
 
 
 #Merge menu
@@ -63,4 +59,6 @@ mergeMenu.addCommand('From', 'nuke.createNode("Merge2", "operation from")', "alt
 
 #Bring back some legacy nodes
 nuke.menu('Nodes').addCommand('Shuffle','nuke.createNode("Shuffle")',"h")
+
+
 
